231067436533b9ee2b13a4eb9c8d51f6933a984d
Sssnake <nechsimo23@gmail.com>
2023-10-07 02:04:29 +0000

    test

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { ThemeToggle } from "./theme-toggle"
+
 
 export function SiteHeader() {
   return (
@@ -9,6 +11,8 @@ export function SiteHeader() {
           <div>
             <div className='container mx-3 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0 '>
               <div>
+              <ThemeToggle />
+
                 <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
               </div>
               <div className='flex flex-1 items-center justify-end space-x-4'>
