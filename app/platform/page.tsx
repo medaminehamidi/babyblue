db618150bd6afc97edb3b3276cfec1ae32d8b190
Sssnake <nechsimo23@gmail.com>
2023-10-24 03:32:11 +0000

    add progress to limit

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -1,17 +1,67 @@
+'use client'
 import Monthlytrans from "@/components/monthly-trans";
+import { AlertOctagon } from "lucide-react";
+import { useBalanceStore, useTransactionStore } from "../store";
+import { useEffect, useState } from "react";
+import { Progress } from "@/components/ui/progress";
+const calculateTotalTransactions = (transactions: any) => {
+  const totalBills = transactions.reduce((total: number, transaction: any) => {
+    
+    return total + transaction.amount
+  }, 0)
+  return totalBills
+}
 export default function Home() {
+  const [first, setFirst] = useState(0)
+  
+  
+  const balance = useBalanceStore((state) => state.balance)
+  const [domLoaded, setDomLoaded] = useState(false);
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+  const transactions = useTransactionStore((state) => state.transaction)
+  const value = calculateTotalTransactions(transactions)
+const perce = (value/balance.income*100)
+console.warn(perce)
+useEffect(() => {
+  if(balance && balance.income && transactions.length) {
+    setFirst((value/balance.income*100))
+  }
+
+ 
+}, [domLoaded])
   return (
     <div className="w-full grid grid-cols-5">
       <div className="col-span-3 border-r border-black p-6">
         <div>
           <h1 className="text-2xl">Transactions</h1>
         </div>
-        <Monthlytrans month={new Date}/>
-        <Monthlytrans month={new Date}/>
-        <Monthlytrans month={new Date}/>
-        <Monthlytrans month={new Date}/>
-        <Monthlytrans month={new Date}/>
+        <Monthlytrans month={new Date} />
+        <Monthlytrans month={new Date} />
+        <Monthlytrans month={new Date} />
+        <Monthlytrans month={new Date} />
+        <Monthlytrans month={new Date} />
 
+      </div>
+      <div className="col-span-2 p-6">
+        <h1 className="text-2xl">Spending Limits</h1>
+        <div className="mt-4 w-full">
+          <p className="text-sm text-gray-600">Monthly Transaction Limit</p>
+          {domLoaded && <div className="flex items-end mt-2">
+            <p className="font-bold text-lg leading-5">{calculateTotalTransactions(transactions)} DH</p>
+            <p className="text-sm text-gray-600 ml-1">of {balance.income} DH</p>
+          </div>}
+          <div className="w-full flex items-center mt-2 justify-between">
+          { <Progress value={first} className="w-[80%] h-2"/>}
+
+            {domLoaded &&<p className="text-sm">{(calculateTotalTransactions(transactions)/balance.income*100).toFixed(0)}%</p>}
+          </div>
+          <div className="flex items-center mt-2">
+            <AlertOctagon className="w-4 h-4 text-red-600"/>
+            <p className="text-xs ml-2 text-red-600">Your spending limit has almost reached the limit!</p>
+          </div>
+        </div>
       </div>
     </div>
 
--- components/monthly-trans.tsx
+++ components/monthly-trans.tsx
@@ -1,5 +1,11 @@
+'use client'
 import TransactionC2 from "@/components/transaction-c2";
 import moment from 'moment'
+import { TransactionCard } from "./transaction-card";
+import { useTransactionStore } from "@/app/store";
+import { toast } from "./ui/use-toast";
+import { useSupabase } from "./SupabaseSessionProvider";
+import { useEffect, useState } from "react";
 type MonthlytransProps = {
     month: Date,
 }
@@ -31,11 +37,33 @@ const transactionS = [
 
 ]
 export default function Monthlytrans({ month }: MonthlytransProps) {
+    const [domLoaded, setDomLoaded] = useState(false);
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+    const transactions = useTransactionStore((state) => state.transaction)
+    const updateTransaction = useTransactionStore((state) => state.updateTransaction)
+    const { supabase } = useSupabase()
+
+    const Remove = async (index: number, id: any) => {
+        if (transactions.length > 1) {
+          transactions.splice(index, 1)
+          updateTransaction([...transactions])
+        } else {
+          updateTransaction([])
+        }
+        const { error } = await supabase
+          .from('transactions')
+          .delete()
+          .eq('id', id)
+        toast({
+          description: 'Transaction Removed successfully'
+        })
+      }
     return (
         <div className="w-full rounded-md">
             <h1 className="font-light text-lg mt-4">{moment(month).format("D MMMM, YYYY")} </h1>
-            {transactionS.map((item,key) => <TransactionC2 beneficiary={item.beneficiary} key={key} date={item.date} subCat={item.subCat} amount={item.amount} />)}
-        </div>
+            {domLoaded && <div>{transactions.map((item, key) => <TransactionCard title={item.title} amount={item.amount} key={key} type={item.type} date={item.date} description={item.description} index={key.toString()} Remove={() => Remove(key, item.id)} />)}</div>} </div>
 
 
 
