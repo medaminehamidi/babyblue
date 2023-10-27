06acfa20456e546175de7007cf8cc1252f442d3f
amine <amine@alphorm.com>
2023-10-27 02:34:49 +0100

    fix deployment

--- components/monthly-trans.tsx
+++ components/monthly-trans.tsx
@@ -7,65 +7,99 @@ import { toast } from "./ui/use-toast";
 import { useSupabase } from "./SupabaseSessionProvider";
 import { useEffect, useState } from "react";
 type MonthlytransProps = {
-    month: Date,
+  month: Date,
 }
 const transactionS = [
-    {
-        beneficiary: "fiat",
-        date: new Date,
-        subCat: "Fashion",
-        amount: 200.00
-    },
-    {
-        beneficiary: "fiat",
-        date: new Date,
-        subCat: "Fashion",
-        amount: 200.00
-    },
-    {
-        beneficiary: "fiat",
-        date: new Date,
-        subCat: "Fashion",
-        amount: 200.00
-    },
-    {
-        beneficiary: "fiat",
-        date: new Date,
-        subCat: "Fashion",
-        amount: 200.00
-    },
+  {
+    beneficiary: "fiat",
+    date: new Date,
+    subCat: "Fashion",
+    amount: 200.00
+  },
+  {
+    beneficiary: "fiat",
+    date: new Date,
+    subCat: "Fashion",
+    amount: 200.00
+  },
+  {
+    beneficiary: "fiat",
+    date: new Date,
+    subCat: "Fashion",
+    amount: 200.00
+  },
+  {
+    beneficiary: "fiat",
+    date: new Date,
+    subCat: "Fashion",
+    amount: 200.00
+  },
 
 ]
 export default function Monthlytrans({ month }: MonthlytransProps) {
-    const [domLoaded, setDomLoaded] = useState(false);
+  const [domLoaded, setDomLoaded] = useState(false);
   useEffect(() => {
     setDomLoaded(true);
   }, []);
-    const transactions = useTransactionStore((state) => state.transaction)
-    const updateTransaction = useTransactionStore((state) => state.updateTransaction)
-    const { supabase } = useSupabase()
+  const transactions = useTransactionStore((state) => state.transaction)
+  const updateTransaction = useTransactionStore((state) => state.updateTransaction)
+  const { supabase } = useSupabase()
 
-    const Remove = async (index: number, id: any) => {
-        if (transactions.length > 1) {
-          transactions.splice(index, 1)
-          updateTransaction([...transactions])
-        } else {
-          updateTransaction([])
-        }
-        const { error } = await supabase
-          .from('transactions')
-          .delete()
-          .eq('id', id)
-        toast({
-          description: 'Transaction Removed successfully'
-        })
-      }
-    return (
-        <div className="w-full rounded-md">
-            <h1 className="font-light text-lg mt-4">{moment(month).format("D MMMM, YYYY")} </h1>
-            {domLoaded && <div>{transactions.map((item, key) => <TransactionCard title={item.title} amount={item.amount} key={key} type={item.type} date={item.date} description={item.description} index={key.toString()} Remove={() => Remove(key, item.id)} />)}</div>} </div>
+  const Remove = async (index: number, id: any) => {
+    if (transactions.length > 1) {
+      transactions.splice(index, 1)
+      updateTransaction([...transactions])
+    } else {
+      updateTransaction([])
+    }
+    const { error } = await supabase
+      .from('transactions')
+      .delete()
+      .eq('id', id)
+    toast({
+      description: 'Transaction Removed successfully'
+    })
+  }
+  const UpdateTransaction = async (index: any, id: any) => {
+    const result = await supabase
+      .from('transactions')
+      .update({ isUpcoming: false })
+      .eq('id', id)
+      .select()
+    if (result?.data && result?.data.at(0)) {
+      transactions.splice(index, 1)
+      updateTransaction([{
+        id: result?.data.at(0).id,
+        amount: result?.data.at(0).amount,
+        title: result?.data.at(0).category,
+        type: result?.data.at(0).sent,
+        date: result?.data.at(0).date,
+        description: result?.data.at(0).beneficiary || 'Empty',
+        isUpcoming: result?.data.at(0).isUpcoming
+      }, ...transactions,])
+    }
+    toast({
+      description: 'Transaction Updated successfully'
+    })
+  }
+  return (
+    <div className="w-full rounded-md">
+      <h1 className="font-light text-lg mt-4">{moment(month).format("D MMMM, YYYY")} </h1>
+      {domLoaded && <div>{transactions.map((item, key) => <TransactionCard
+        title={item.title}
+        amount={item.amount}
+        isUpcoming={item.isUpcoming}
+        key={key}
+        type={item.type}
+        date={item.date}
+        description={item.description}
+        index={key.toString()}
+        UpdateTransaction={() => UpdateTransaction(key, item.id)}
+        Remove={() => Remove(key, item.id)}
+      />)}</div>}
+    </div>
 
 
 
-    )
+  )
 }
